f20c10eeedc5e5fd5613d7e8f1171ebb1e6699ce
obie rustemi <orustemi@uber.com>
2025-08-21 19:12:39 -0400

    Working Looker Studio visualization for Tim

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -5,7 +5,7 @@
 
 set -e  # Exit on any error
 
-BUCKET="obie-cp-gantt"
+BUCKET="obie-ls-cp-gantt-plugin"
 SOURCE_DIR="."
 FILES=("cp-gantt.css" "cp-gantt.js" "cp-gantt-config.json" "manifest.json" "index.html")
 
